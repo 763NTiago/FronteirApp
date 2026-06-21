8dea985e319500970fb9a01f48a7f7e24d84fbd6
763ntiago <763ntiago@gmail.com>
2026-06-20 23:17:21 -0400

    feat: adiciona entidade Service com relacionamento ManyToOne e precisão financeira BigDecimal

--- src/main/java/br/com/sttalis/fronteirapp/domain/model/Service.java
+++ src/main/java/br/com/sttalis/fronteirapp/domain/model/Service.java
@@ -46,7 +46,7 @@ public class Service {
     @JoinColumn(name = "provider_id", nullable = false)
     private User provider;
 
-    @Column(name - "created_at", nullable = false, updatable = false)
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAT;
 
     protected Service () {}
